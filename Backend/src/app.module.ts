e7357de69af7903847a80a95ddde8c8f3c1f654d
san ats <san@gmail.com>
2026-03-19 20:30:55 -0500

    delete notification logic for api rest

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -6,11 +6,10 @@ import { SessionsModule } from './api/sessions/sessions.module';
 import { AttendanceModule } from './api/attendance/attendance.module';
 import { ClinicalNotesModule } from './api/clinical-notes/clinical-notes.module';
 import { ReportsModule } from './api/reports/reports.module';
-import { NotificationsModule } from './api/notifications/notifications.module';
 
 
 
 @Module({
-  imports: [UsersModule, PatientsModule, TherapistsModule, SessionsModule, AttendanceModule, ClinicalNotesModule, ReportsModule, NotificationsModule],
+  imports: [UsersModule, PatientsModule, TherapistsModule, SessionsModule, AttendanceModule, ClinicalNotesModule, ReportsModule],
 })
 export class AppModule {}
